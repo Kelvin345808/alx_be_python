c350bce5afe652a227c421fe9892530693e95ea9
kelvin nzau <kelvinnzau10@gmail.com>
2024-12-15 05:25:18 -0800

    add basic_operations.py script

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,12 @@
-number1 = 10 
-number2 = 5 
-addition = number1 + number2 
-subtraction = number1 - number2 
-multipication = number1 * number2 
-print(f"(addition) of (number1) and (number2) is (addition)") 
-print(f"(subtraction) of (number1) and (number2) is (subtraction)") 
-print(f"(multipication) of (number1) and (number2) is (multipication)")
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Output results
+print(f"Addition of {number1} and {number2} is {addition}.")
+print(f"Subtraction of {number1} and {number2} is {subtraction}.")
+print(f"Multiplication of {number1} and {number2} is {multiplication}.")
